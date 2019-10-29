98377f6d69e1c45e301e2637d7c948a0d3cfb399
ZHOUZYH0 <zychina66@163.com>
2019-10-29 18:48:19 +0800

    11

--- src/com/zgl/Servlet.java
+++ src/com/zgl/Servlet.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Servlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        System.out.println("11");
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
